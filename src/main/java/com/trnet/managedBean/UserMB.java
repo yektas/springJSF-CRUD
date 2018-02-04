797f4d01267d0708a351e8d1f35136890a3fd971
yektas <ahmetsercanyektas@gmail.com>
2018-02-04 16:18:39 +0300

    i18n Added.

---
+++ src/main/java/com/trnet/managedBean/UserMB.java
@@ -0,0 +1,26 @@
+package com.trnet.managedBean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import java.util.Locale;
+
+@ManagedBean(name = "userMB")
+@SessionScoped
+public class UserMB {
+
+    private Locale locale;
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public String getLanguage() {
+        return locale.getLanguage();
+    }
+
+    public void changeLanguage(String language) {
+        locale = new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale(language));
+    }
+}
