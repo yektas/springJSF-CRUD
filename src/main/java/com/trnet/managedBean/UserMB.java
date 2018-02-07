44100060be887ccc9236decc8ad2b145cf05e2d3
yektas <ahmetsercanyektas@gmail.com>
2018-02-07 15:16:43 +0300

    User locale warning issue solved

--- src/main/java/com/trnet/managedBean/UserMB.java
+++ src/main/java/com/trnet/managedBean/UserMB.java
@@ -10,14 +10,9 @@ import java.util.Locale;
 
 @ManagedBean(name = "userMB")
 @SessionScoped
-public class UserMB implements Serializable {
+public class UserMB {
 
-    private Locale locale;
-
-    private static final long serialVersionUID = 1L;
-
-    private static final Logger logger = Logger.getLogger(CarMB.class);
-
+    private Locale locale = new Locale("tr");
 
     public Locale getLocale() {
         return locale;
@@ -29,15 +24,6 @@ public class UserMB implements Serializable {
 
     public void changeLanguage(String language) {
         locale = new Locale(language);
-
-        // logs debug
-        if (logger.isDebugEnabled()) {
-            logger.debug("UserMB.process()");
-        }
-
-        // logs exception
-        logger.error("This is Error message for UserMB", new Exception("UserMB Logs"));
-
         FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale(language));
     }
 }
